c28bdec1741109840037bc414dbbb28dc2411022
leolind <leolind@umich.edu>
2024-06-01 22:02:15 -0700

    Availability check for times within the same day

--- components/ProfilePage/AvailabilityCalendar/AvailabilityModal.tsx
+++ components/ProfilePage/AvailabilityCalendar/AvailabilityModal.tsx
@@ -6,8 +6,14 @@ import {convertHextoAvailArray, indexmapToLocaltime, timeslotToUTCstr} from "@/u
 // Datetime objects in the calendar will be displayed in the client's local time zone.
 // The datetime objects and availability hexstrings will be stored in the backend in UTC time.
 
+// Check if two dates are the same
+function isSameDay(date1: Date, date2: Date): boolean {
+  return date1.getFullYear() === date2.getFullYear() &&
+         date1.getMonth() === date2.getMonth() &&
+         date1.getDate() === date2.getDate();
+}
 // Filter the availability based on the type of session
-function filterAvailability(availability: number[], typeofSession: string | null): number[] {
+function filterAvailability(availability: number[], typeofSession: string | null, selectedDate: Date): number[] {
   let lengthOfSession = 1;
   // resume session is 15 min long, interview session is 45 min long, and the default session is 15 min long
   if (typeofSession === 'resume') {
@@ -15,7 +21,11 @@ function filterAvailability(availability: number[], typeofSession: string | null
   } else if (typeofSession === 'interview') {
     lengthOfSession = 3;
   }
-
+  const now = new Date();
+  const currentHour = now.getHours();
+  const currentMin = now.getMinutes();
+  const formattedTime = (currentHour * 4) + Math.floor(currentMin / 15)
+  console.log(isSameDay(now, selectedDate))
   return availability.map((time, index) => {
     if (time === 1) { // If the current slot is available
       // eslint-disable-next-line no-plusplus
@@ -23,6 +33,9 @@ function filterAvailability(availability: number[], typeofSession: string | null
         if (availability[index + i] === 0) { // If any of these slots is not available
           return 0; // Mark the current slot as unavailable
         }
+      }
+      if(isSameDay(now, selectedDate) && index < formattedTime){
+        return 0;
       }
       return 1; // Otherwise, the slot is available
     }
@@ -54,7 +67,7 @@ export function AvailabilityModal({
     };
   }, []);
 
-  const availability = filterAvailability(convertHextoAvailArray(threedayAvailability), typeofSession);
+  const availability = filterAvailability(convertHextoAvailArray(threedayAvailability), typeofSession, date);
   const rows = availability.map((time, index) => (
     time === 1 && (
       <Grid.Col span={4} key={index}>
